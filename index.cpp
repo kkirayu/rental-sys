a11616f13ce59829dacacf58ac1c636742f86bfb
naufal murtadho <naufalmurtadho202@gmail.com>
2025-05-27 19:28:59 +0700

    Update index.cpp
    coba coba doang nil

--- index.cpp
+++ index.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+
+struct RentalKendaraan
+{
+    string nama;
+    int unit, tanggalpeminjaman, tanggalpengembalian;
+    RentalKendaraan *next;
+} ;
+
 int main() {
+    RentalKendaraan *node1, *node2, *node3;
+    
     cout << "Hello, World!" << endl;
     return 0;
 }
